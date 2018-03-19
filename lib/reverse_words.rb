9789fb1e565092de5daf624ef98d13f33311b1c9
Zheng Cao <chori84@gmail.com>
2018-03-18 19:03:36 -0700

    Implement reverse words and pass all tests

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,28 @@
 # A method to reverse each word in a sentence, in place.
 def reverse_words(my_words)
-  raise NotImplementedError
+  return nil if my_words.nil?
+  return my_words if my_words.length == 0 || my_words.length == 1
+
+  index = 0
+  while index < my_words.length
+    j = index
+    while my_words[j] != " " && my_words[j] != nil
+      j += 1
+    end
+
+    # my_words[index..(j-1)]
+    if j != index
+      m = index
+      n = j - 1
+      while m < n
+        temp = my_words[m]
+        my_words[m] = my_words[n]
+        my_words[n] = temp
+        m += 1
+        n -= 1
+      end
+    end
+
+    index = j + 1
+  end
 end
